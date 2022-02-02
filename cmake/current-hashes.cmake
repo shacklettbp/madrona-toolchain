53a86c2e5d399bdb6d7a7a5df90c52b596cc7417
shacklettbp <shacklettbp@users.noreply.github.com>
2022-02-02 11:08:44 +0000

    Update hashes

--- cmake/current-hashes.cmake
+++ cmake/current-hashes.cmake
@@ -1,2 +1,2 @@
-set(MADRONA_DEPS_LINUX_HASH "")
-set(MADRONA_DEPS_MAC_HASH "")
+set(MADRONA_DEPS_LINUX_HASH "cf47037948ce723af4680df7c601a8c9d2a101cb172270e7c43d92187f6381e6  madrona-deps-65dd44b-ubuntu-latest.tar.zst")
+set(MADRONA_DEPS_MAC_HASH "ba07034c38077b4ff1d27f2895e9b2953312bb12fdcd2a50a87dd83de5c52abd  madrona-deps-65dd44b-macos-latest.tar.zst")
