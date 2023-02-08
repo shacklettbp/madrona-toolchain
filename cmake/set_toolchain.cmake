c55cc98cb456e221de915e1a29f0e2fdd36cb95d
Brennan Shacklett <bpshacklett@gmail.com>
2023-02-08 11:25:14 -0800

    Update toolchain extension

--- cmake/set_toolchain.cmake
+++ cmake/set_toolchain.cmake
@@ -38,13 +38,13 @@ function(madrona_setup_toolchain)
         endif()
     endif()
     
-    set(DEPS_URL "https://github.com/shacklettbp/madrona-toolchain/releases/download/${MADRONA_TOOLCHAIN_VERSION}/madrona-toolchain-${MADRONA_TOOLCHAIN_VERSION}-${TOOLCHAIN_OS_NAME}.tar.zst")
+    set(DEPS_URL "https://github.com/shacklettbp/madrona-toolchain/releases/download/${MADRONA_TOOLCHAIN_VERSION}/madrona-toolchain-${MADRONA_TOOLCHAIN_VERSION}-${TOOLCHAIN_OS_NAME}.tar.xz")
     
     FetchContent_Declare(MadronaBundledToolchain
         URL ${DEPS_URL}
         URL_HASH SHA256=${MADRONA_TOOLCHAIN_HASH}
         DOWNLOAD_DIR "${TOOLCHAIN_REPO}/download"
-        DOWNLOAD_NAME cur.tar # Can't name it .tar.zst or CMake will ignore
+        DOWNLOAD_NAME cur.tar # Can't name it .tar.xz or CMake will ignore
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
         SOURCE_DIR "${TOOLCHAIN_REPO}/bundled-toolchain"
     )
