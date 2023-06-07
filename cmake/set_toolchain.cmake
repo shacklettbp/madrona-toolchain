fac268948ae8e15dde78561fd2cfed9214c8c263
Brennan Shacklett <bpshacklett@gmail.com>
2023-06-06 19:34:28 -0700

    libtool fix for madrona projects as well

--- cmake/set_toolchain.cmake
+++ cmake/set_toolchain.cmake
@@ -66,6 +66,21 @@ function(madrona_setup_toolchain)
 
     set(CMAKE_C_COMPILER "${TOOLCHAIN_SYSROOT}/bin/clang" CACHE STRING "")
     set(CMAKE_CXX_COMPILER "${TOOLCHAIN_SYSROOT}/bin/clang++" CACHE STRING "")
+
+    # On macos, universal builds with this toolchain will be broken due to
+    # llvm-ranlib not working with univeral libraries. /usr/bin/ar is picked
+    # by default by cmake, but the compiler ranlib is still used, breaking
+    # static libraries. 
+    # One option is to force /usr/bin/ranlib on macos.
+    # The better option is to do what LLVM itself does, which is to just use
+    # libtool on macos for building static libraries since it is more
+    # optimized anyway: https://reviews.llvm.org/D19611. llvm-libtool
+    # correctly handles universal binaries
+    if (MADRONA_MACOS)
+        set(CMAKE_CXX_CREATE_STATIC_LIBRARY "\"${TOOLCHAIN_PATH}/bin/llvm-libtool\" -static -no_warning_for_no_symbols -o <TARGET> <LINK_FLAGS> <OBJECTS>")
+        # need to disable ranlib or it will run after libtool
+        set(CMAKE_RANLIB "")
+    endif ()
 endfunction()
 
 madrona_setup_toolchain()
