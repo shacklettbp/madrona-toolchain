bbb60de1f0396ff17251cdaa914eb8fc7500f6fe
Brennan Shacklett <bpshacklett@gmail.com>
2024-06-22 12:38:16 -0700

    New libcxx with full exception support

---
+++ src/op_newdel/op_newdel.cpp
@@ -0,0 +1,141 @@
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <new>
+
+namespace {
+
+[[noreturn]] void oom(size_t num_bytes)
+{
+    fprintf(stderr, "OOM: %zu\n", num_bytes);
+    exit(EXIT_FAILURE);
+}
+
+inline void * opNewImpl(size_t num_bytes) noexcept
+{
+    void *ptr = malloc(num_bytes);
+    if (!ptr) [[unlikely]] {
+        oom(num_bytes);
+    }
+
+    return ptr;
+}
+
+inline void opDeleteImpl(void *ptr) noexcept
+{
+    free(ptr);
+}
+
+inline void * opNewAlignImpl(size_t num_bytes, std::align_val_t al) noexcept
+{
+    size_t alignment = static_cast<size_t>(al);
+    if (alignment < sizeof(void *)) {
+        alignment = sizeof(void *);
+    }
+
+    static_assert(sizeof(al) == sizeof(size_t));
+    void *ptr = aligned_alloc(alignment, num_bytes);
+    if (!ptr) [[unlikely]] {
+        oom(num_bytes);
+    }
+
+    return ptr;
+}
+
+inline void opDeleteAlignImpl(void *ptr, std::align_val_t al) noexcept
+{
+    (void)al;
+    free(ptr);
+}
+
+}
+
+void * operator new(size_t num_bytes)
+{
+    return ::opNewImpl(num_bytes);
+}
+
+void operator delete(void *ptr) noexcept
+{
+    ::opDeleteImpl(ptr);
+}
+
+void * operator new(
+    size_t num_bytes, const std::nothrow_t &) noexcept
+{
+    return ::opNewImpl(num_bytes);
+}
+
+void operator delete(
+    void *ptr, const std::nothrow_t &) noexcept
+{
+    ::opDeleteImpl(ptr);
+}
+
+void * operator new[](size_t num_bytes)
+{
+    return ::opNewImpl(num_bytes);
+}
+
+void operator delete[](void *ptr) noexcept
+{
+    ::opDeleteImpl(ptr);
+}
+
+void * operator new[](
+    size_t num_bytes, const std::nothrow_t &) noexcept
+{
+    return ::opNewImpl(num_bytes);
+}
+
+void operator delete[](
+    void *ptr, const std::nothrow_t &) noexcept
+{
+    ::opDeleteImpl(ptr);
+}
+
+// Aligned versions
+
+void * operator new(size_t num_bytes, std::align_val_t al)
+{
+    return ::opNewAlignImpl(num_bytes, al);
+}
+
+void operator delete(void *ptr, std::align_val_t al) noexcept
+{
+    ::opDeleteAlignImpl(ptr, al);
+}
+
+void * operator new(
+    size_t num_bytes, std::align_val_t al, const std::nothrow_t &) noexcept
+{
+    return ::opNewAlignImpl(num_bytes, al);
+}
+
+void operator delete(
+    void *ptr, std::align_val_t al, const std::nothrow_t &) noexcept
+{
+    ::opDeleteAlignImpl(ptr, al);
+}
+
+void * operator new[](size_t num_bytes, std::align_val_t al)
+{
+    return ::opNewAlignImpl(num_bytes, al);
+}
+
+void operator delete[](void *ptr, std::align_val_t al) noexcept
+{
+    ::opDeleteAlignImpl(ptr, al);
+}
+
+void * operator new[](
+    size_t num_bytes, std::align_val_t al, const std::nothrow_t &) noexcept
+{
+    return ::opNewAlignImpl(num_bytes, al);
+}
+
+void operator delete[](
+    void *ptr, std::align_val_t al, const std::nothrow_t &) noexcept
+{
+    ::opDeleteAlignImpl(ptr, al);
+}
